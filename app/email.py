c0a954769b33743b0b7254861362550b0623de36
kuboy8 <860365755@qq.com>
2018-08-15 20:13:21 -0700

    chapter8

--- app/email.py
+++ app/email.py
@@ -12,7 +12,7 @@ def send_email(to, subject, template, **kwargs):
     msg = Message(current_app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,\
                   sender=current_app.config['FLASKY_MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
-    msg.html = render_template(template + '.html', **kwargs)
+    #msg.html = render_template(template + '.html', **kwargs)
     #mail.send(msg)
     thr = Thread(target=send_async_email, args=[app, msg])
     thr.start()
--- config.py
+++ config.py
@@ -6,7 +6,8 @@ class Config:
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <'+os.environ.get('MAIL_USERNAME')+'>'
+    #FLASKY_MAIL_SENDER = 'Flasky Admin <'+os.environ.get('MAIL_USERNAME')+'>'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <xxx@example.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     
     @staticmethod
